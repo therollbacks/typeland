3ab302438489602ede703e77052acdf1064354e6
Yuyu Lai <yuyulai97@gmail.com>
2019-04-06 15:50:44 -0700

    chomp screen

--- components/chompcomponent.js
+++ components/chompcomponent.js
@@ -1,43 +1,26 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import { Root } from "native-base";
-import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableOpacity, ImageBackground, Image } from 'react-native';
 import { Font, AppLoading } from "expo";
 import { createStackNavigator } from 'react-navigation';
 
 export default class ChompComponent extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //    this.state = { loading: true };
-    
-
-  // }
-
-
-  // _onPressButton() {
-  //   Alert.alert('You tapped the button!')
-  // }
-
-  // render() {
-    
-  //   return (
-  //     <View>
-  //     <Text style = {styles.openText}> Main </Text>
-  //     </View> 
-  //   );
-  // }
 
   render() {
+     let imageGameBack = require("./gameback.jpg");
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Chomp Screen</Text>
+        <ImageBackground source={imageGameBack} style={{width: '100%', height: '100%'}}>
+
+        <Text>Add game screen here</Text>
         
-        {/* Look here! We "push" the Details route */}
         
         <Button
           title="Exit"
           onPress={() => this.props.navigation.push('Details')}
         />
+        </ImageBackground>
       </View>
     );
   }
